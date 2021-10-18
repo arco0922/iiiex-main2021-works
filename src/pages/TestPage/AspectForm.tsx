9f43734108b4c17eb2844f5a0c2afab4aaa15237
arco0922 <k9o2u2ta@icloud.com>
2021-10-18 14:40:20 +0900

    [AspectForm.tsx] del: delete "確定" button

--- src/pages/TestPage/AspectForm.tsx
+++ src/pages/TestPage/AspectForm.tsx
@@ -10,22 +10,30 @@ export const AspectForm: React.VFC<Props> = ({ setAspectRatio }) => {
   const [aspectHeight, setAspectHeight] = React.useState<number>(9);
   const [isAspectWidthValid, setIsAspectWidthValid] = React.useState<boolean>(true);
   const [isAspectHeightValid, setIsAspectHeightValid] = React.useState<boolean>(true);
+
+  React.useEffect(() => {
+    if (isAspectWidthValid && isAspectHeightValid) {
+      setAspectRatio(aspectHeight / aspectWidth);
+    }
+  }, [aspectWidth, aspectHeight, isAspectWidthValid, isAspectHeightValid, setAspectRatio]);
+
   const aspectWidthHandler = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setAspectWidth(Number(e.target.value));
-    Number(e.target.value) > 0 ? setIsAspectWidthValid(true) : setIsAspectWidthValid(false);
+    if (Number(e.target.value) > 0) {
+      setIsAspectWidthValid(true);
+      setAspectWidth(Number(e.target.value));
+    } else {
+      setIsAspectWidthValid(false);
+    }
   }, []);
   const aspectHeightHandler = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setAspectHeight(Number(e.target.value));
-    Number(e.target.value) > 0 ? setIsAspectHeightValid(true) : setIsAspectHeightValid(false);
+    if (Number(e.target.value) > 0) {
+      setIsAspectHeightValid(true);
+      setAspectHeight(Number(e.target.value));
+    } else {
+      setIsAspectHeightValid(false);
+    }
   }, []);
 
-  const setAspectRatioHandler = React.useCallback(
-    (e: React.MouseEvent<HTMLButtonElement>) => {
-      e.preventDefault();
-      isAspectWidthValid && isAspectHeightValid ? setAspectRatio(aspectHeight / aspectWidth) : console.log('Error');
-    },
-    [aspectHeight, aspectWidth, isAspectHeightValid, isAspectWidthValid, setAspectRatio],
-  );
   return (
     <StyledForm>
       <StyledInputSection>
@@ -33,7 +41,6 @@ export const AspectForm: React.VFC<Props> = ({ setAspectRatio }) => {
         <StyledInput type="text" onChange={aspectWidthHandler} defaultValue="16"></StyledInput>
         <StyledSpan> : </StyledSpan>
         <StyledInput type="text" onChange={aspectHeightHandler} defaultValue="9"></StyledInput>
-        <StyledButton onClick={setAspectRatioHandler}>確定</StyledButton>
       </StyledInputSection>
       <StyledDialog isShow={!(isAspectWidthValid && isAspectHeightValid)}>正の数値を入力して下さい</StyledDialog>
     </StyledForm>
@@ -69,20 +76,6 @@ const StyledInput = styled.input`
   margin-right: 10px;
   padding: 0 3px;
   text-align: center;
-`;
-
-const StyledButton = styled.button`
-  padding: 0px 10px;
-  outline: none;
-  border: none;
-  border-radius: 3px;
-  font-weight: bold;
-  background-color: #303030;
-  color: white;
-  &:hover {
-    background-color: #000000;
-    cursor: pointer;
-  }
 `;
 
 interface StyledDialogProps {
