c1dda6708ff9a7b91f0dfeb3f1238764689642f1
arco0922 <k9o2u2ta@icloud.com>
2021-10-20 03:04:20 +0900

    [TopPage.tsx] add: import WorksListSketch into TopPage

--- src/pages/TopPage/TopPage.tsx
+++ src/pages/TopPage/TopPage.tsx
@@ -1,9 +1,16 @@
+import { WorksListSketch } from 'components/Sketches/WorksListSketch';
 import React from 'react';
+import styled from 'styled-components';
 
 export const TopPage: React.VFC = () => {
   return (
-    <div>
-      <p>トップページ</p>
-    </div>
+    <StyledContainer>
+      <WorksListSketch width="100%" height="100%"></WorksListSketch>
+    </StyledContainer>
   );
 };
+
+const StyledContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
