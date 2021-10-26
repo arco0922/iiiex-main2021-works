b582e93db42de2a54130a2c4248daa003538513b
arco0922 <k9o2u2ta@icloud.com>
2021-10-27 01:42:09 +0900

    [WorksInfo.ts] add: create works info array

---
+++ src/constants/WorksInfo.ts
@@ -0,0 +1,78 @@
+export interface WorksInfo {
+  id: number;
+  title: string;
+  thumbnailName: string;
+}
+
+export const worksInfoArr: WorksInfo[] = [
+  {
+    id: 0,
+    title: 'offline on line',
+    thumbnailName: '0_nakagawa.jpg',
+  },
+  {
+    id: 1,
+    title: '僕と、生きる',
+    thumbnailName: '1_miyake.jpg',
+  },
+  {
+    id: 2,
+    title: 'ゼロマインド～0歳児パンク～',
+    thumbnailName: '2_baby-thumbnail.jpg',
+  },
+  {
+    id: 3,
+    title: 'on my . (オン マイ ピリオド)',
+    thumbnailName: '3_mochizuki.jpg',
+  },
+  {
+    id: 4,
+    title: '#FiK握手会',
+    thumbnailName: '4_ueno_thumbnail.jpg',
+  },
+  {
+    id: 5,
+    title: 'puppeTuber',
+    thumbnailName: '5_aramaki_puppeTuber.jpg',
+  },
+  {
+    id: 6,
+    title: '(s)hukan',
+    thumbnailName: '6_(s)hukan.jpg',
+  },
+  {
+    id: 7,
+    title: '確率であそぼ、',
+    thumbnailName: '7_kurata_picture.jpg',
+  },
+  {
+    id: 8,
+    title: 'ENDRAGON',
+    thumbnailName: '8_ENDRAGON.jpg',
+  },
+  {
+    id: 9,
+    title: 'KABUKU!',
+    thumbnailName: '9_kabuku_tmb800.jpg',
+  },
+  {
+    id: 10,
+    title: 'I-mage',
+    thumbnailName: '10_I-mage.jpg',
+  },
+  {
+    id: 11,
+    title: 'Home-Gallery',
+    thumbnailName: '11_Siyuan_Home-Gallery.jpg',
+  },
+  {
+    id: 12,
+    title: 'Tap Your Life',
+    thumbnailName: '12_mikami_tapyourlife.jpg',
+  },
+  {
+    id: 13,
+    title: 'Animal clock',
+    thumbnailName: '13_akiyama.jpg',
+  },
+];
