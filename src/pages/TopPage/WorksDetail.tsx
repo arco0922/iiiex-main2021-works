bd36c1831eb7a6cdfa169636bd9216c81ccd9c1c
arco0922 <k9o2u2ta@icloud.com>
2021-10-27 09:16:07 +0900

    [WorksDetail.tsx] feat: implement basic detail

--- src/pages/TopPage/WorksDetail.tsx
+++ src/pages/TopPage/WorksDetail.tsx
@@ -1,23 +1,73 @@
+import { worksInfoArr } from 'constants/WorksInfo';
 import React from 'react';
 import styled from 'styled-components';
 
-export const WorksDetail: React.VFC = () => {
+interface Props {
+  hoverId: number;
+}
+
+export const WorksDetail: React.VFC<Props> = ({ hoverId }) => {
+  const info = React.useMemo(() => worksInfoArr.filter((worksInfo) => worksInfo.id === hoverId)[0], [hoverId]);
   return (
     <StyledContainer>
-      <StyledTitle>INFO</StyledTitle>
+      <StyledTitle>作品詳細</StyledTitle>
+      <StyledDetailContainer>
+        <StyledThumbnail src={`/static/assets/thumbnails/${info?.thumbnailName}`} width="100%"></StyledThumbnail>
+        <StyledSection>
+          <h4>作品名</h4>
+          <p>{info.title}</p>
+        </StyledSection>
+        <StyledSection>
+          <h4>制作者</h4>
+          <p>だれだれ</p>
+        </StyledSection>
+        <StyledSection>
+          <h4>作品説明</h4>
+          <p>キャプション</p>
+        </StyledSection>
+        <StyledSection>
+          <h4>近くにある作品</h4>
+          <p>作品作品</p>
+        </StyledSection>
+      </StyledDetailContainer>
     </StyledContainer>
   );
 };
 
 const StyledContainer = styled.div`
-  min-width: 250px;
+  min-width: 300px;
+  width: 300px;
   height: 100%;
   padding: 5px;
   background-color: #141414;
   color: white;
+  display: flex;
+  flex-direction: column;
 `;
 
 const StyledTitle = styled.h2`
-  font-size: 18px;
+  min-height: 24px;
+  width: 95%;
+  font-size: 16px;
   font-weight: 400;
+  border-bottom: 1px solid white;
+  margin-bottom: 3px;
+`;
+
+const StyledDetailContainer = styled.div`
+  flex: 1;
+  width: 95%;
+  overflow-y: auto;
+`;
+
+const StyledThumbnail = styled.img`
+  margin-top: 5px;
+  margin-bottom: 15px;
+`;
+
+const StyledSection = styled.section`
+  margin-bottom: 15px;
+  & > h4 {
+    margin-bottom: 3px;
+  }
 `;
