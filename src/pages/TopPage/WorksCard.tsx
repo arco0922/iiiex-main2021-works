cb05c65aedd1039a20055c01bc6d47ef02294fab
arco0922 <k9o2u2ta@icloud.com>
2021-10-29 15:58:11 +0900

    [WorksCard.tsx] feat: add link to works page

--- src/pages/TopPage/WorksCard.tsx
+++ src/pages/TopPage/WorksCard.tsx
@@ -1,5 +1,7 @@
+import { theme } from 'constants/Theme';
 import { WorksInfo } from 'constants/WorksInfo';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 interface Props {
@@ -25,7 +27,12 @@ export const WorksCard: React.VFC<Props> = ({ worksInfo, selectId, setSelectId }
         src={`/static/assets/thumbnails/${worksInfo.thumbnailBaseName}.jpg`}
         height="100%"
       ></StyledThumbnail>
-      <p>{worksInfo.title}</p>
+      <StyledInfoDiv>
+        <p>{worksInfo.title}</p>
+        <StyledButton>
+          <StyledLink to={`/works/${worksInfo.id}`}>この作品を見る</StyledLink>
+        </StyledButton>
+      </StyledInfoDiv>
     </StyledContainer>
   );
 };
@@ -41,7 +48,7 @@ const StyledContainer = styled.div<StyledContainerProps>`
   border-bottom: 1px solid #2a2a2a;
   padding-top: 1px;
   padding-bottom: 1px;
-  border: ${({ isSelect }) => (isSelect ? '3px solid rgba(255,100,100,1)' : '')};
+  border: ${({ isSelect }) => (isSelect ? `3px solid ${theme.color.primary}` : '')};
   &:hover {
     cursor: pointer;
     background-color: #4a4a4a;
@@ -50,4 +57,34 @@ const StyledContainer = styled.div<StyledContainerProps>`
 
 const StyledThumbnail = styled.img`
   margin-right: 5px;
+`;
+
+const StyledInfoDiv = styled.div`
+  position: relative;
+  flex: 1;
+  height: 100%;
+  & > p {
+    font-size: 18px;
+  }
+`;
+
+const StyledButton = styled.button`
+  position: absolute;
+  bottom: 5px;
+  right: 5px;
+  background-color: ${theme.color.primary};
+  color: white;
+  text-decoration: none;
+  outline: none;
+  border: none;
+  padding: 3px;
+  border-radius: 3px;
+  &:hover {
+    background-color: ${theme.color.activePrimary};
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: white;
 `;
