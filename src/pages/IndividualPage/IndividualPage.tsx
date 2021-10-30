00080e6d5914f7d32791521377dfe58166a1bf08
shuto ogihara <yahoopuyo@gmail.com>
2021-10-30 16:30:44 +0900

    [IndividualPage] feat: back to home btn

--- src/pages/IndividualPage/IndividualPage.tsx
+++ src/pages/IndividualPage/IndividualPage.tsx
@@ -1,7 +1,10 @@
 import { Header } from 'components/Header/Header';
+import { theme } from 'constants/Theme';
 import { worksInfoArr } from 'constants/WorksInfo';
 import React from 'react';
 import { RouteComponentProps, useParams, withRouter } from 'react-router';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import { IndividualWorksWindow } from './IndividualWorksWindow';
 
 interface Params {
@@ -22,8 +25,29 @@ const IndividualPageComponent: React.VFC<RouteComponentProps<Params>> = ({ match
       <Header />
       <p>作品ページ：作品IDは{worksId}</p>
       <IndividualWorksWindow srcUrl={worksInfo.srcUrlPc} iframeHeight={iframeHeight} iframeWidth={iframeWidth} />
+      <StyledButton>
+        <StyledLink to="/">作品一覧へ</StyledLink>
+      </StyledButton>
     </div>
   );
 };
 
 export const IndividualPage = withRouter(IndividualPageComponent);
+
+const StyledButton = styled.button`
+  background-color: ${theme.color.primary};
+  color: white;
+  text-decoration: none;
+  outline: none;
+  border: none;
+  padding: 3px;
+  border-radius: 3px;
+  &:hover {
+    background-color: ${theme.color.activePrimary};
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+`;
