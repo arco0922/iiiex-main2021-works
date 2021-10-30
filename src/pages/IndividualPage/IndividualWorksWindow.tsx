df16605ba8396312bff4bd52aaa615e4b0618b97
arco0922 <k9o2u2ta@icloud.com>
2021-10-31 02:08:57 +0900

    [IndividualWorksWindow.tsx] feat: create skeleton UI

--- src/pages/IndividualPage/IndividualWorksWindow.tsx
+++ src/pages/IndividualPage/IndividualWorksWindow.tsx
@@ -11,10 +11,39 @@ interface Props {
 }
 
 export const IndividualWorksWindow: React.VFC<Props> = ({ srcUrl, iframeWidth, iframeHeight }) => {
+  const iframeRef = React.useRef<HTMLIFrameElement>(null);
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+  const loadHandler = React.useCallback(() => setIsLoading(false), [setIsLoading]);
+  React.useEffect(() => {
+    setIsLoading(true);
+    const iframe = iframeRef.current;
+    if (iframe !== null) {
+      iframe.addEventListener('load', loadHandler);
+    }
+    return () => {
+      if (iframe !== null) {
+        iframe.removeEventListener('load', loadHandler);
+      }
+    };
+  }, [srcUrl, loadHandler]);
+
   return (
     <StyledRoot>
       <StyledContainer iframeWidth={iframeWidth} iframeHeight={iframeHeight}>
-        {srcUrl && <StyledIframe src={srcUrl} allow="fullscreen *; autoplay *; camera *; microphone *"></StyledIframe>}
+        {srcUrl ? (
+          <StyledIframeContainer>
+            <StyledIframe
+              src={srcUrl}
+              allow="fullscreen *; autoplay *; camera *; microphone *"
+              ref={iframeRef}
+            ></StyledIframe>
+            <StyledLoading isLoading={isLoading}>Loading...</StyledLoading>
+          </StyledIframeContainer>
+        ) : (
+          <StyledSkeleton>
+            <p>メンテナンス中</p>
+          </StyledSkeleton>
+        )}
       </StyledContainer>
       <StyledButton>
         <StyledLink to="/">作品一覧へ戻る</StyledLink>
@@ -39,6 +68,12 @@ const StyledContainer = styled.div<StyledContainerProps>`
   height: ${({ iframeHeight }) => iframeHeight};
 `;
 
+const StyledIframeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+`;
+
 const StyledIframe = styled.iframe`
   width: 100%;
   height: 100%;
@@ -46,6 +81,32 @@ const StyledIframe = styled.iframe`
   border: none;
   display: block;
   background-color: white;
+`;
+
+interface StyledLoadingProps {
+  isLoading: boolean;
+}
+
+const StyledLoading = styled.div<StyledLoadingProps>`
+  background-color: black;
+  color: white;
+  display: ${({ isLoading }) => (isLoading ? 'flex' : 'none')};
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledSkeleton = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #b4b4b4;
 `;
 
 const StyledButton = styled.button`
