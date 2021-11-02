aa83c81a11cd84f4300dff7256b9f31635c37b3d
arco0922 <k9o2u2ta@icloud.com>
2021-11-02 20:11:46 +0900

    [IndividualPage.tsx] chore: style fix in narrow layout

--- src/pages/IndividualPage/IndividualPage.tsx
+++ src/pages/IndividualPage/IndividualPage.tsx
@@ -24,35 +24,20 @@ const IndividualPageComponent: React.VFC<RouteComponentProps<Params> & Props> = 
   const { height, width } = useWindowDimensions();
   const iframeWidth = isFull ? '100vw' : width < 800 ? '95vw' : 'max(60vw , 500px)';
   const iframeHeight = isFull
-    ? '100vh'
+    ? `calc(100vh - ${headerHeight}px)`
     : `calc( ${iframeWidth} * ${worksInfo.aspectRatio ? worksInfo.aspectRatio : 9 / 16} )`;
 
   React.useEffect(() => {
     setSelectId(worksId);
   }, [worksId, setSelectId]);
 
-  if (width < 800) {
-    return (
-      <StyledRoot>
-        <Header showNavigationToTop={true} isFull={isFull} setIsFull={setIsFull} />
-        <StyledContentContainer isFull={isFull}>
-          <IndividualWorksWindow
-            srcUrl={isMobile ? worksInfo.srcUrlSp : worksInfo.srcUrlPc}
-            iframeHeight={iframeHeight}
-            iframeWidth={iframeWidth}
-            isFull={isFull}
-            setIsFull={setIsFull}
-          />
-          {!isFull && <IndividualWorksCaption worksInfo={worksInfo} />}
-        </StyledContentContainer>
-      </StyledRoot>
-    );
-  }
+  const isNarrowLayout = width < 800;
+
   return (
     <StyledRoot>
       <Header showNavigationToTop={true} isFull={isFull} setIsFull={setIsFull} />
       <StyledContentContainer isFull={isFull}>
-        <StyledWorksContainer>
+        <StyledWorksContainer isNarrowLayout={isNarrowLayout}>
           <IndividualWorksWindow
             srcUrl={isMobile ? worksInfo.srcUrlSp : worksInfo.srcUrlPc}
             iframeHeight={iframeHeight}
@@ -87,8 +72,12 @@ const StyledContentContainer = styled.div<StyledContentContainerProps>`
   overflow-y: auto;
 `;
 
-const StyledWorksContainer = styled.div`
-  height: 100%;
+interface StyledWorksContainerProps {
+  isNarrowLayout: boolean;
+}
+
+const StyledWorksContainer = styled.div<StyledWorksContainerProps>`
   display: flex;
   justify-content: center;
+  flex-direction: ${({ isNarrowLayout }) => (isNarrowLayout ? 'column' : 'row')};
 `;
