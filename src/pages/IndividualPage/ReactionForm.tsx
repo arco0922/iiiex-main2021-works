cfe4fee5aca778f96c44a3824f1fc2e11981c7c2
arco0922 <k9o2u2ta@icloud.com>
2021-11-15 02:49:07 +0900

    [ReactionForm.tsx] fix: fix form bug in iOS

--- src/pages/IndividualPage/ReactionForm.tsx
+++ src/pages/IndividualPage/ReactionForm.tsx
@@ -31,8 +31,8 @@ export const ReactionForm: React.VFC<Props> = ({ worksId, isNarrowLayout }) => {
       <StyledFormWrapper isSent={isSent}>
         <StyledForm action={REACTIONFORM_URL} ref={formRef} method="post" target="dummyIframe">
           <StyledTextArea name={worksInfo.formEntry} ref={textRef}></StyledTextArea>
-          <StyledSubmitButton type="button" value="送信" onClick={submitForm}></StyledSubmitButton>
         </StyledForm>
+        <StyledSubmitButton onClick={submitForm}>送信</StyledSubmitButton>
         <StyledDummyIframe name="dummyIframe" />
       </StyledFormWrapper>
       <StyledSentMessage isSent={isSent}>回答ありがとうございます</StyledSentMessage>
@@ -53,7 +53,9 @@ interface StyledFormWrapperProps {
 }
 
 const StyledFormWrapper = styled.div<StyledFormWrapperProps>`
-  display: ${({ isSent }) => (isSent ? 'none' : 'block')};
+  display: ${({ isSent }) => (isSent ? 'none' : 'flex')};
+  flex-direction: column;
+  align-items: center;
 `;
 
 const StyledTitle = styled.h4`
@@ -78,9 +80,10 @@ const StyledTextArea = styled.textarea`
   resize: none;
   border-radius: 5px;
   margin-bottom: 10px;
+  font-size: 16px;
 `;
 
-const StyledSubmitButton = styled.input`
+const StyledSubmitButton = styled.button`
   outline: none;
   border: none;
   border-radius: 5px;
