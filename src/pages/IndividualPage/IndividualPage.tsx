a987a9dcad0850a9328430de0df567a3139a0986
shuto ogihara <yahoopuyo@gmail.com>
2021-11-17 19:27:59 +0900

    [individualPage] show srcUrlSp for mobile and tablet

--- src/pages/IndividualPage/IndividualPage.tsx
+++ src/pages/IndividualPage/IndividualPage.tsx
@@ -8,7 +8,7 @@ import { IndividualWorksDetail } from './IndividualWorksDetail';
 import { IndividualWorksWindow } from './IndividualWorksWindow';
 import { OwnQuestionnaire } from './OwnQuestionnaire';
 import { ReactionForm } from './ReactionForm';
-import { isMobile } from 'react-device-detect';
+import { isMobile, isTablet } from 'react-device-detect';
 import { Visited } from 'AppRoot';
 import { LayoutType } from 'constants/Layout';
 import { Coord, MapModeId } from 'constants/MapCoords';
@@ -35,6 +35,8 @@ interface Props {
   setWorksHistory: (worksHistory: number[]) => void;
   mapModeId: MapModeId;
 }
+
+const touchable = isMobile || isTablet;
 
 const IndividualPageComponent: React.VFC<RouteComponentProps<Params> & Props> = ({
   match,
@@ -153,13 +155,13 @@ const IndividualPageComponent: React.VFC<RouteComponentProps<Params> & Props> = 
                 />
               )}
               <IndividualWorksWindow
-                srcUrl={isMobile ? worksInfo.srcUrlSp : worksInfo.srcUrlPc}
+                srcUrl={touchable ? worksInfo.srcUrlSp : worksInfo.srcUrlPc}
                 iframeHeight={iframeHeight}
                 iframeWidth={iframeWidth}
                 isFull={isFull}
                 setIsFull={setIsFull}
                 isNarrowLayout={isNarrowLayout}
-                isShowButtonOnly={!isFull && isMobile && worksInfo.isSmartphoneFullscreenOnly === true}
+                isShowButtonOnly={!isFull && touchable && worksInfo.isSmartphoneFullscreenOnly === true}
               />
               {!isFull && <IndividualWorksDetail worksInfo={worksInfo} isNarrowLayout={isNarrowLayout} />}
               {!isFull && <ReactionForm worksId={worksId} isNarrowLayout={isNarrowLayout} />}
