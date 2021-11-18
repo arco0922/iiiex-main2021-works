a345f33ed5168b003140152b04a2411dae16e5a0
Kanazawanaoaki <naoaki65k@gmail.com>
2021-11-19 03:11:49 +0900

    [webpack, index, useTracking] remove comment out

--- src/hooks/useTracking.ts
+++ src/hooks/useTracking.ts
@@ -18,7 +18,6 @@ export const useTracking = (trackingId: string | undefined): void => {
       }
       if (!window.gtag) return;
       if (!trackingId) {
-        // console.log('Tracking not enabled, as `trackingId` was not given and there is no `GA_MEASUREMENT_ID`.');
         return;
       }
       window.gtag('config', trackingId, { page_path: location.pathname });
