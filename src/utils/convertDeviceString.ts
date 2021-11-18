4c5029fe4ec25fc4b52bc447fbfea670e9a5889e
shuto ogihara <yahoopuyo@gmail.com>
2021-11-19 03:44:43 +0900

    [topWorksDetail,convertDeviceString] fix: size of device info in top page

--- src/utils/convertDeviceString.ts
+++ src/utils/convertDeviceString.ts
@@ -3,10 +3,10 @@ import { Device } from 'constants/WorksInfo';
 export const convertDeviceString = (device: Device): string => {
   switch (device) {
     case 'PC':
-      return 'PCのみ';
+      return 'PC 対応';
     case 'SP':
-      return 'スマホのみ';
+      return 'スマホ・タブレット 対応';
     case 'BOTH':
-      return 'PC・スマホどちらも対応';
+      return 'PC・スマホ・タブレット 対応';
   }
 };
